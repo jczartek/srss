66ba54ce3f608f710466d9fc80cce1df8434b53f
Jakub Czartek <kuba@linux.pl>
2017-05-19 14:16:42 +0100

    ...

---
+++ lib/srss/srss.rb
@@ -0,0 +1,75 @@
+module SRSS
+  module Signalable
+
+    class NoBlockError < Exception
+    end
+
+    def connect(name, &blk)
+      unless block_given?
+        raise NoBlockError, "When connecting signal must given a block!"
+      end
+
+      @_signal_connections ||= []
+      @_next_connection_id ||= 1
+
+      id = @_next_connection_id
+      @_next_connection_id = @_next_connection_id + 1
+
+      @_signal_connections.push({
+        :id           => id,
+        :name         => name,
+        :callback     => blk,
+        :blocked => false
+      })
+
+      id
+    end
+
+    def disconnect(id)
+      raise ArgumentError, "No signal handler #{id} found" unless @_signal_connections
+
+      @_signal_connections.reject! {|handler|; handler[:id] == id; }
+    end
+
+    def disconnect_all()
+      @_signal_connections = []
+      @_next_connection_id = 1
+    end
+
+    def block(id)
+      handler = @_signal_connections.detect { |handler| handler[:id] == id }
+
+      unless handler.nil?
+        handler[:blocked] = true
+      end
+    end
+
+    def unblock(id)
+      handler = @_signal_connections.detect { |handler| handler[:id] == id }
+
+      unless handler.nil?
+        handler[:blocked] = false
+      end
+    end
+
+    def emit(name, *args)
+      return unless @_signal_connections
+
+      handlers = @_signal_connections.select {|handler| name == handler[:name]}
+
+      args_handler = [self]
+      args_handler.concat(args) if args.any?
+
+      handlers.each do |handler;ret|
+        unless handler[:blocked]
+          begin
+          ret = handler[:callback].call(*args_handler)
+          break if ret == true
+          rescue => e
+            puts e.message
+          end
+        end
+      end
+    end
+  end
+end
---
+++ lib/srss/version.rb
@@ -0,0 +1,3 @@
+module SRSS
+  VERSION = "0.1.0".freeze
+end
